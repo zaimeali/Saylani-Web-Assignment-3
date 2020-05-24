61463e5e733f4a1be254477aaa2b32a4f19f5c45
Zaime Ali <zaimeali1997@gmail.com>
2020-05-24 13:42:53 +0500

    Done with Cart Functionality in JS

--- js/app.js
+++ js/app.js
@@ -6,18 +6,29 @@ function deleteCart(cart_id) {
 
         if(cart.style.display !== 'none'){
             cart.style.display = "none";
+
+            if(cart_id === 'cart_1'){
+                changeValue('cart-product-1', 'cart-quantity-1', 'cartProductPrice1', true);
+            }
+            else{
+                changeValue('cart-product-2', 'cart-quantity-2', 'cartProductPrice2', true);
+            }
         }
     }
 }
 
 
-function changeValue(price, quantity, sub) {
+function changeValue(price, quantity, sub, flag = false) {
     let productQuantity = document.getElementById(quantity).value;
 
     let productPrice = document.getElementById(price).innerHTML;
 
     if(productPrice.includes(',')){
         productPrice = productPrice.replace(",", "");
+    }
+
+    if(flag){
+        productQuantity = 0;
     }
 
     let subPrice = parseInt(productPrice) * parseInt(productQuantity);
